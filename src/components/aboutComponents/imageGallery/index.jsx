a3501f2b47c17f0f937cd490bd99ea999c9c7f33
Raoof-e-Services <raoofeservices@gmail.com>
2023-10-02 13:13:12 +0500

    about page completed

---
+++ src/components/aboutComponents/imageGallery/index.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import {Image, Row, Col } from 'antd';
+import { useMediaQuery } from 'react-responsive';
+import PrimaryButton from '../../common/buttons/primary';
+
+const ImageGallery = ({images}) => {
+  const isMobile = useMediaQuery({
+    query: '(max-width: 501px)', // Change this breakpoint as needed
+  });
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+  };
+
+  const columnStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: '1',
+    margin: '5px',
+  };
+
+  const rowStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+  };
+
+  const smallBoxStyle = {
+    flex: '1',
+    margin: '5px',
+    
+  };
+
+  const bigBoxStyle = {
+    flex: '3',
+    display: 'flex',
+    flexDirection: 'column',
+    margin: '5px',
+    
+  };
+
+  const halfHeightStyle = {
+    flex: '0.7',
+    margin: '5px',
+    
+  };
+
+  const thirdHeightStyle = {
+    margin: '5px',
+
+    width:"100%"
+  };
+
+  return (
+    <>
+    <Row style={{margin:"5% 1%"}}>
+      <Col  xs={24} sm={24} md={12} lg={12} xl={12} align={isMobile ? "middle" :""}><h1>Our Gallery</h1> <p>We bring to the fore all our expertise to make your journey very comfortable.</p></Col>
+      <Col  xs={24} sm={24} md={12} lg={12} xl={12} align={isMobile ? "middle" :"right"}><PrimaryButton title={"Open Gallery"}/></Col>
+    </Row>
+    <div style={containerStyle}>
+      <div style={rowStyle}>
+        <div style={columnStyle}>
+          <div style={halfHeightStyle}>
+            <Image src={images[0]} width="100%" height="100%"/>
+          </div>
+          <div style={halfHeightStyle}>
+          <Image src={images[1]} width="100%" height="100%"/>
+          </div>
+        </div>
+        <div style={columnStyle}>
+          <div style={smallBoxStyle}>
+          <Image src={images[2]} width="100%" height="100%"/>
+          </div>
+        </div>
+        <div style={{...columnStyle, marginTop:"0.7%"}}>
+          <div style={{height:"68%"}}>
+          <Image src={images[3]} width="100%" height="99%"/>
+          </div>
+          <div style={{height:"40%", marginTop:"5%"}}>
+          <Image src={images[4]} width="100%" height="99%"/>
+          </div>
+        </div>
+        <div style={bigBoxStyle}>
+          <div style={halfHeightStyle}>
+          <Image src={images[5]} width="100%" height="100%"/>
+          </div>
+          <div style={{display:"flex"}}>
+          <div style={thirdHeightStyle}>
+          <Image src={images[6]} width="100%" />
+          </div>
+          <div style={thirdHeightStyle}>
+          <Image src={images[7]} width="100%" />
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default ImageGallery;
--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CircleComponent from "../../components/common/circleImages";
 import ImageCard from "../../components/aboutComponents/teamComponent";
+import { Row, Col,} from 'antd';
 import Team1 from "../../../images/ibrat.png"
 import Team2 from "../../../images/team2.png"
 import Team3 from "../../../images/team3.png"
@@ -9,12 +10,23 @@ import Icon1 from "../../../images/teamicon1.png"
 import Icon2 from "../../../images/teamicon2.png"
 import Icon3 from "../../../images/teamicon3.png"
 import Icon4 from "../../../images/teamicon4.png"
-import { Menu, Row, Col, Drawer, Button, ConfigProvider, Image } from 'antd';
+import Image1 from "../../../images/imageGallery1.png"
+
+import G1 from "../../../images/g1.png"
+import G2 from "../../../images/g2.png"
+import G3 from "../../../images/g3.png"
+import G4 from "../../../images/g4.png"
+import G5 from "../../../images/g5.png"
+import G6 from "../../../images/g6.png"
+import G7 from "../../../images/g7.png"
+import G8 from "../../../images/g8.png"
+
 import withNavbar from "../../components/common/HOCNabar/index";
 import ServicesSection from '../../components/aboutComponents/servicesSection';
 import Footer from "../../components/common/footer";
 import AboutHero from '../../components/aboutComponents/aboutHero';
 import ChooseUs from '../../components/aboutComponents/chooseUs';
+import ImageGallery from '../../components/aboutComponents/imageGallery';
 
 
 const imageCardData = [
@@ -24,6 +36,16 @@ const imageCardData = [
   { imageUrl: Team4, mainText: <div style={{ display: "flex" }}><h3 >Ghazi Raja</h3> &nbsp;&nbsp;&nbsp;<p>Managing Director  </p></div>, rotatedText: <div style={{ display: "flex", justifyContent: "space-evenly" }}><img src={Icon4} alt="icon" /><img src={Icon1} alt="icon" /><img src={Icon2} alt="icon" /><img src={Icon4} alt="icon" /></div> },
 ];
 
+const images = [
+  G1,
+  G2,
+  G3,
+  G4,
+  G5,
+  G6,
+  G7,
+  G8,
+];
 function About() {
   return (
     <>
@@ -48,6 +70,7 @@ function About() {
 
         ))}
       </Row>
+      <ImageGallery images={images} />
       <CircleComponent />
       <Footer />
 
