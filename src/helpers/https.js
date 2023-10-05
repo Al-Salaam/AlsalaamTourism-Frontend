8aebfbec3845f2ad11beace94adc798f594c92b4
abhiimalik <abdullah.223228@gmail.com>
2023-10-05 05:34:05 -0700

    done

--- src/helpers/https.js
+++ src/helpers/https.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const https = axios.create({
-    baseURL: "http://localhost:8000/api/v1", 
+    baseURL: "https://lonely-long-johns-crow.cyclic.app/api/v1", 
     withCredentials: true
 })
