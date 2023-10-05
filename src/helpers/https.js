2b163373a0713894df7a2691d8413a6d8166b636
abhiimalik <abdullah.223228@gmail.com>
2023-10-05 05:50:14 -0700

    oka

--- src/helpers/https.js
+++ src/helpers/https.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const https = axios.create({
-    baseURL: `${process.env.URL}/api/v1`, 
+    baseURL: "https://lonely-long-johns-crow.cyclic.app/api/v1", 
     withCredentials: true
 })
