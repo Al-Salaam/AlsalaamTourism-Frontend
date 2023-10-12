bbca27059ed872833e24d2b622b5dc10fcb614bf
Raoof-e-Services <raoofeservices@gmail.com>
2023-10-12 17:38:04 +0500

    404 Completed

--- src/components/common/activitySlider/index.jsx
+++ src/components/common/activitySlider/index.jsx
@@ -16,7 +16,7 @@ const ActivitySlider = ({data}) => {
     const [currentPage, setCurrentPage] = useState(0);
   
     const handleNextClick = () => {
-        if (currentPage + 1 < Math.ceil(data.length / itemsPerPage)) {
+        if (currentPage + 1 < Math.ceil(data?.length / itemsPerPage)) {
             setCurrentPage(currentPage + 1);
         }
     };
@@ -30,7 +30,7 @@ const ActivitySlider = ({data}) => {
     const renderCards = () => {
         const startIndex = currentPage * itemsPerPage;
         const endIndex = startIndex + itemsPerPage;
-        const cardsToShow = data && data.slice(startIndex, endIndex);
+        const cardsToShow = data && data?.slice(startIndex, endIndex);
 
         const cardStyle = {
             boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', // Add the box shadow style here
@@ -42,7 +42,7 @@ const ActivitySlider = ({data}) => {
         return (
             <Row gutter={20} >
                 
-                {cardsToShow.map((item) => (
+                {cardsToShow?.map((item) => (
                     <Col align="middle" key={item._id} xs={24} sm={24} md={12} lg={8} xl={8} xxl={8}>
                         <FlipCard style={{ border: "red 1px solid" }} imageSrc={item?.images[0]?.url} title1={item.name} rate={item.ratings} price={item.price} shortdescription={item.shortdescription} activityId={item._id} noOfReviews={item.noOfReviews}/>
                     </Col>
@@ -63,7 +63,7 @@ const ActivitySlider = ({data}) => {
                 <Button onClick={handlePreviousClick} disabled={currentPage === 0} style={{ backgroundColor: "white", color: "3B505A", borderRadius: "100%", fontSize: "50px", border: "none" }}>
                     <ArrowLeftOutlined />
                 </Button>
-                <Button onClick={handleNextClick} disabled={currentPage + 1 >= Math.ceil(data.length / itemsPerPage)} style={{ backgroundColor: "white", color: "3B505A", borderRadius: "100%", fontSize: "50px", border: "none" }}>
+                <Button onClick={handleNextClick} disabled={currentPage + 1 >= Math.ceil(data?.length / itemsPerPage)} style={{ backgroundColor: "white", color: "3B505A", borderRadius: "100%", fontSize: "50px", border: "none" }}>
                     <ArrowRightOutlined />
                 </Button>
             </Col>
