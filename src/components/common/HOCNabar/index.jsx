1f7f0f2ac2f367a444b8c0955492de1e67259685
Raoof-e-Services <raoofeservices@gmail.com>
2023-10-13 16:16:14 +0500

    navbar completed with drawer

--- src/components/common/HOCNabar/index.jsx
+++ src/components/common/HOCNabar/index.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../Navbar';
+import { useMediaQuery } from 'react-responsive';
 
 const withNavbar = (WrappedComponent) => {
   return function WithNavbar(props) {
@@ -18,36 +19,41 @@ const withNavbar = (WrappedComponent) => {
       setShowOverlay(false);
     };
 
+    const isManue = useMediaQuery({
+      query: '(max-width: 1198px)', // Change this breakpoint as needed
+    });
+
+    const isSmall = useMediaQuery({
+      query: '(max-width: 380px)', // Change this breakpoint as needed
+    });
+
     // Styling
     const styles = {
-      box: {
+      overlay: {
         position: 'fixed',
-        top: '64px', // Adjust this value according to your Navbar's height
-        right: '250px',
-        width: '400px',
-        height: '300px',
-        backgroundColor: '#FFFFFF',
-        color: 'black',
-        zIndex: 999,
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent black
+        zIndex: 4, // Place it below the box
       },
-      button:{
-        backgroundColor:"none",
-        border:"none",
-        position:"absolute",
-        bottom:"10px",
-        right:"10px"
-      }
+      box: { position: 'fixed', top: '64px', right: isManue ? "-120px" : '50px', width: isSmall ? "90vw" : '400px', height: '300px', backgroundColor: '#FFFFFF', color: 'black', margin: "0 150px", zIndex: 5, borderRadius: "10px" },
+      button: { backgroundColor: "#3B505A", border: "none", position: "absolute", bottom: "10px", right: "10px", color: "white", borderRadius: "40%", height: "30px", width: "30px" }
     }
-
 
     return (
       <div>
         <Navbar showOverlayMessage={showOverlayMessage} />
         {showOverlay && (
-          <div style={styles.box}
-          >
-            {overlayContent}
-            <button style={styles.button} onClick={hideOverlay}>X</button>
+          <div>
+            <div style={styles.overlay} onClick={hideOverlay}></div>
+            <div id="overlay-box" style={styles.box}>
+              {overlayContent}
+              <button style={styles.button} onClick={hideOverlay}>
+                X
+              </button>
+            </div>
           </div>
         )}
         <WrappedComponent {...props} />
