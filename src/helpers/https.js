d6a55f2afc636d9ae34c0af1e57464b50e1a93d0
hackedzero7 <121494420+hackedzero7@users.noreply.github.com>
2023-10-27 19:47:31 +0500

    for deployment

--- src/helpers/https.js
+++ src/helpers/https.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 export const https = axios.create({
-    baseURL: "http://localhost:8000/api/v1",
+    baseURL: "https://al-salaam-tourism-c93fa215ea59.herokuapp.com/api/v1",
     withCredentials: true
 })
