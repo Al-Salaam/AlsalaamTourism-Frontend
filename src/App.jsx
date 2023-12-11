3bb503adacf454be72f3909dbbd370f4b6e04c9d
hackedzero7 <121494420+hackedzero7@users.noreply.github.com>
2023-12-11 11:34:27 +0500

    commented desticnation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/authetication/login/index";
-import Destinations from "./pages/destinations/index";
+// import Destinations from "./pages/destinations/index";
 import About from "./pages/about";
-import Dubai from "./pages/destinations/dubai";
-import AbuDhabi from "./pages/destinations/abuDhabi";
-import Fujairah from "./pages/destinations/fujairah";
-import Sharjah from "./pages/destinations/sharjah";
-import RasAlKhaima from "./pages/destinations/RasAlKhaima";
+// import Dubai from "./pages/destinations/dubai";
+// import AbuDhabi from "./pages/destinations/abuDhabi";
+// import Fujairah from "./pages/destinations/fujairah";
+// import Sharjah from "./pages/destinations/sharjah";
+// import RasAlKhaima from "./pages/destinations/RasAlKhaima";
 import { Toaster } from "react-hot-toast";
 import ActivityPage from "./pages/activityPage";
 import SingleActivity from "./pages/singleActivity";
@@ -118,12 +118,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/activity" element={<ActivityPage />} />
         <Route path="/activity/:slug" element={<SingleActivity />} />
-        <Route path="/destinations" element={<Destinations />} />
+        {/* <Route path="/destinations" element={<Destinations />} />
         <Route path="/dubai" element={<Dubai />} />
         <Route path="/abuDhabi" element={<AbuDhabi />} />
         <Route path="/fujairah" element={<Fujairah />} />
         <Route path="/sharjah" element={<Sharjah />} />
-        <Route path="/rasAlKhaima" element={<RasAlKhaima />} />
+        <Route path="/rasAlKhaima" element={<RasAlKhaima />} /> */}
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/packages" element={<Packages />} />
